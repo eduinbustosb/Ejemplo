e057816e6753f0a95239a2df8def2a8280d1bf7e
ESTUDIANTE <ESTUDIANTE@S4-16>
2017-08-16 07:33:09 -0500

    probando el repositorio Git programa de Holaaaa, opraciones con binarios

--- 20170814/src/pkg20170814/Main.java
+++ 20170814/src/pkg20170814/Main.java
@@ -5,6 +5,8 @@
  */
 package pkg20170814;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ESTUDIANTE
@@ -16,10 +18,12 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo...!");
+        /*System.out.println("Hola mundo...!");
         
         
         //OPERACIONES DE BITS
+        System.out.println("");
+        System.out.println("<<<< OPERACIONES DE BITS >>>>");
         // &  = AND
         // |  = OR
         // ^  = XOR
@@ -37,14 +41,92 @@ public class Main {
         System.out.println(b);
         
         
-        //CICLOS FOR         
-        int i;
+        //CICLOS FOR
+        System.out.println("");
+        System.out.println("<<<< CICLO FOR >>>>");
+        
+        int i;        
         for(i=0; i<5;i++) {
+            if(i == 3)
+                continue;
             System.out.println(i);
-            break;
         }
         System.out.println(i);
         
+        System.out.println("");
+        System.out.println("<<<< CICLO WHILE >>>>");
+        
+        i = 2;
+        while (i<10) {
+            System.out.println(i);
+            i+=2;
+        }
+        
+        System.out.println("");
+        System.out.println("<<<< CICLO DO WHILE >>>>");
+        
+        i = 3;
+        do {
+            System.out.println(i);
+            i += 3;
+        } while (i<15);
+        
+        switch (i) {
+            
+        }*/
+        
+        System.out.println("<<<< PROGRAMA QUE LEE UN NUMERO ENTERO Y DETERMINA SI SUS DIGITOS ESTAN EN ORDEN ASCENDENTE O NO >>>>");
+        System.out.println("");
+        
+        Scanner vscan = new Scanner(System.in); 
+        
+        int digi;
+        System.out.println("Introduzca el tamaño en digitos para el numero: ");
+        digi = vscan.nextInt();
+        
+        int n;
+        
+        int lon, ban = 0;
+        String cad_valor;
+        do {
+            System.out.println("Introduzca un número entero de "+digi+" cifras: ");
+            n = vscan.nextInt();
+            
+            lon = Integer.toString(n).length();
+            if(lon == digi)
+                ban = 1;
+            else 
+                System.out.println("Numero Incorrecto...!");
+                        
+        } while (ban == 0);
+        
+        
+        
+        int max = 100;
+        int tipo = 0;
+        int res;
+        int temp;
+        temp = n;
+        do {
+            res = n % 10;
+            n = n / 10;
+            //System.out.println(n);
+            if(res < max) {
+                tipo = 1;
+                max = res;
+            } else {
+                tipo = 2;
+                n=0;
+            }
+        } while (n>0);
+        
+        String cadena;
+        if(tipo == 1)
+            cadena = "Esta Ordenado Ascendentemente";
+        else
+            cadena = "NO esta Ordenado Ascendentemente";
+        
+        System.out.println("El numero '"+temp+"' "+cadena);
     }
     
 }
